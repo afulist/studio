99ce9b18ba4c04fec07d99d12a9308d68dbe8e26
afulist <afulist@cht.com.tw>
2016-01-25 10:42:17 +0800

    init proj

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('travel')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();

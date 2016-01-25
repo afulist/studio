bf82c74e264df3b9b743bb3714d3b8b36b71b685
afulist <afulist@cht.com.tw>
2016-01-25 15:04:09 +0800

    test v0.0.1

---
+++ gulp/tag.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp');
+var tag_version = require('gulp-tag-version');
+
+// Assuming there's "version: 1.2.3" in package.json,
+// tag the last commit as "v1.2.3"
+gulp.task('tag', function() {
+  return gulp.src(['./package.json']).pipe(tag_version());
+});

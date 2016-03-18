9903d70929500fd7ec8833a5b176e46ade50dc80
afulist <afulist@cht.com.tw>
2016-03-18 16:59:43 +0800

    1.add login button 2.add location list 3.TODO-ui: test Item Menu

--- src/app/home/home.left.controller.js
+++ src/app/home/home.left.controller.js
@@ -14,6 +14,11 @@
 
     // ==view func==
     vm.init = init;
+    vm.locations = [
+      { name: '台灣', img: 'img/100-0.jpeg', newMessage: true },
+      { name: 'Mary Johnson', img: 'img/100-1.jpeg', newMessage: false },
+      { name: 'Peter Carlssonsssssssssssssssssssss', img: 'img/100-2.jpeg', newMessage: false }
+    ];
 
     // ==init func==
     init();

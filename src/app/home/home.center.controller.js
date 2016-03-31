cafc05dcea11d7b492c601dcae0381608e24ce19
afulist <afulist@cht.com.tw>
2016-03-31 14:16:16 +0800

    fix last

--- src/app/home/home.center.controller.js
+++ src/app/home/home.center.controller.js
@@ -13,11 +13,11 @@
     var url = 'https://material.angularjs.org/1.0.6/img/list/60.jpeg';
     vm.userImage = {'background-image':'url('+ url +')'};
     vm.plansSort = [
-      { id: 1, name: '最新在最前'},
-      { id: 2, name: '最多讚最前'},
-      { id: 3, name: '最多收藏最前'},
-      { id: 4, name: '最多天在最前'},
-      { id: 5, name: '最少天在最前'}
+      { id: 1, name: '看最新'},
+      { id: 2, name: '看最多讚'},
+      { id: 3, name: '看最多收藏'},
+      { id: 4, name: '看最多天'},
+      { id: 5, name: '看最少天'}
       //{ id: 1, name: '最多留言最前'}
     ];
     vm.selectedSort = { id: 1, name: '最新在最前'};

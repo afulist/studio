72af98599987874b54ce03ba19217a0aef4ce64e
afulist <afulist@cht.com.tw>
2016-05-04 15:10:16 +0800

    test fb plugin: Deferred long-running timer task(s) to improve scrolling smoothness. See crbug.com/574343.

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -14,12 +14,10 @@
     firebase.root = 'https://pytrip-dev.firebaseio.com/';
     firebase.secret = 'wDPbV8cdgaTvyjFnYoXUhFbP3t4OsW0sl7tLAjVl';
     /* @endif */
-
     /* @if ENV='stage' **
     // Can switch to dev if needed
     firebase.root = 'https://pytrip.firebaseio.com/';
     /* @endif */
-
     /* @if ENV='prod' **
     firebase.root = 'https://pytrip.firebaseio.com/';
     /* @endif */
